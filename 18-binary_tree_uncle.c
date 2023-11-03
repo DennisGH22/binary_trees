4492c89a8aaf54333d4531156d0f68cf8c935c41
Tebogo Ndaba <dennistebogo@gmail.com>
2023-11-03 03:54:03 +0200

    Binary tree

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -17,8 +17,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 	if (grandparent->left == node->parent)
 		return (grandparent->right);
+
 	else if (grandparent->right == node->parent)
 		return (grandparent->left);
+
 	else
 		return (NULL);
 }
